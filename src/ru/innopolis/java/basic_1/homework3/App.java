842e23293d84debaeef025e1e82dc01f1306cb0d
First Last <email@somewhere.com>
2024-06-16 16:05:37 +0300

    Commit third homework

---
+++ src/ru/innopolis/java/basic_1/homework3/App.java
@@ -0,0 +1,26 @@
+package ru.innopolis.java.basic_1.homework3;
+
+import java.util.Scanner;
+
+/*
+    Реализовать класс Телевизор. У класса есть поля, свойства и методы.
+    Проверить работу в классе App, методе main.
+    Обратить внимание на переопределение метода toString.
+*/
+
+public class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        TV samsungTv = new TV("Samsung", 50000, "43\"");
+        System.out.println("\n" + samsungTv + "\n");
+
+        TV lgTv = new TV("LG", "45\"");
+        lgTv.setCost(74989.99);
+        System.out.println(lgTv.getCost() + " - is LG's cost\n");
+
+        TV sonyTv = new TV("Sony", "50\"");
+        System.out.print("Enter how much is Sony Television: ");
+        sonyTv.setCost(scanner.nextDouble());
+        System.out.println(sonyTv);
+    }
+}
